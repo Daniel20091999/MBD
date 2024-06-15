4f60e111f7ff55898f18c35b573e4d41df579135
Daniel García Muñoz <dgm20091999@gmail.com>
2024-06-15 18:51:03 +0200

    git commit -m "creamos funcion de mostrar los datos de una empresa en concreto y la opcion de modificarlos"

--- src/app.py
+++ src/app.py
@@ -31,8 +31,85 @@ class app:
         #Cerramos la conexion para que consuma recursos inecesarios
         self.conn.close()
         
+        
+    #definimos una funcion para mostrar los datos de la tabla t_empresa
+    def mostrar_empresas(self):
+        sql = "select * from t_empresa"
+        self.cursor.execute(sql)
+        empresas = self.cursor.fetchall()
+        print("Empresas existentes\n")
+        for empresa in empresas:
+            print("Nombre empresa: ", empresa[1])
+    
+    #definimos una funcion para mostrar los datos de solo una empresa
+    def mostrar_datos_empresa(self, nombre_empresa):
+        sql = "select * from t_empresa where nombre_empresa = '{}'".format(nombre_empresa) 
+        self.cursor.execute(sql)
+        empresas = self.cursor.fetchall()
+        print("Los datos de la empresa son los siguientes: \n")
+        for empresa in empresas:
+            print(".- 1 Nombre empresa: ", empresa[1])
+            print(".- 2 CIF empresa: ", empresa[2])
+            print(".- 3 Localidad: ", empresa[3])
+            print(".- 4 Direccion: ", empresa[4])
+            print(".- 5 Persona de Contacto: ", empresa[5])
+            print(".- 6 Telefono empresa: ", empresa[6])
+
+            
+    #definimos un metodo para actualizador los datos de una empresa
+    def actualizar_empresa(self):
+        self.mostrar_empresas()
+        nombre_empresa_seleccionada = str(input("Introduce el nombre la empresa que quieres modificar: "))
+        self.mostrar_datos_empresa(nombre_empresa_seleccionada)
+        dato_a_modificar = int(input("¿Qué dato quieres eliminar?(1-6): "))
+        if dato_a_modificar == 1:
+            nuevo_nombre = str(input("Introduce el nuevo nombre de la empresa: "))
+            consulta_sql = "update t_empresa set nombre_empresa = '{}' where  nombre_empresa = '{}'".format(nuevo_nombre, nombre_empresa_seleccionada)
+            self.cursor.execute(consulta_sql)
+            print("Nombre de la empresa actualizado correctamente")
+            self.conn.commit()
+            self.conn.close()
+        if dato_a_modificar == 2:
+            nuevo_cif = str(input("Introduce el nuevo CIF de la empresa: "))
+            consulta_sql = "update t_empresa set cif_empresa = '{}' where  nombre_empresa = '{}'".format(nuevo_cif, nombre_empresa_seleccionada)
+            self.cursor.execute(consulta_sql)
+            print("CIF de la empresa actualizado correctamente")
+            self.conn.commit()
+            self.conn.close()
+        if dato_a_modificar == 3:
+            nueva_localidad = str(input("Introduce la nueva localidad de la empresa: "))
+            consulta_sql = "update t_empresa set localid_empresa = '{}' where  nombre_empresa = '{}'".format(nueva_localidad, nombre_empresa_seleccionada)
+            self.cursor.execute(consulta_sql)
+            print("Localidad de la empresa actualizado correctamente")
+            self.conn.commit()
+            self.conn.close()
+        if dato_a_modificar == 4:
+            nueva_direccion = str(input("Introduce la nueva direccion de la empresa: "))
+            consulta_sql = "update t_empresa set direccion_empresa = '{}' where  nombre_empresa = '{}'".format(nueva_direccion, nombre_empresa_seleccionada)
+            self.cursor.execute(consulta_sql)
+            print("Direccion de la empresa actualizado correctamente")
+            self.conn.commit()
+            self.conn.close()
+        if dato_a_modificar == 5:
+            nuevo_contacto = str(input("Introduce el nombre de la nueva persona de contacto de la empresa: "))
+            consulta_sql = "update t_empresa set contacto_empresa = '{}' where  nombre_empresa = '{}'".format(nuevo_contacto, nombre_empresa_seleccionada)
+            self.cursor.execute(consulta_sql)
+            print("Persona de contacto de la empresa actualizado correctamente")
+            self.conn.commit()
+            self.conn.close()
+        if dato_a_modificar == 6:
+            nuevo_telefono= int(input("Introduce el nuevo telefono de la empresa: "))
+            consulta_sql = "update t_empresa set telefono_empresa = '{}' where  nombre_empresa = '{}'".format(nuevo_telefono, nombre_empresa_seleccionada)
+            self.cursor.execute(consulta_sql)
+            print("Telefono de contacto de la empresa actualizado correctamente")
+            self.conn.commit()
+            self.conn.close()
+            
+            
+         
+        
 aplicacion = app()
 
-aplicacion.añadir_empresa()
+aplicacion.actualizar_empresa()
         
         
